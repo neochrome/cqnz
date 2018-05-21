29344d2bc5ca224b52fee971976c677c34393c24
neochrome <johan@stenqvist.net>
2018-05-21 13:12:26 +0200

    Switched to electron

--- app.js
+++ app.js
@@ -1,54 +1,105 @@
 #!/usr/bin/env node
 'use strict';
-var program = require('commander');
-var path    = require('path');
-var fs      = require('fs');
-var util    = require('util');
-var spawn   = require('child_process').spawn;
-var nw      = require('nodewebkit').findpath();
+const program = require('commander');
+const path    = require('path');
+const fs      = require('fs');
+const { app, BrowserWindow } = require('electron');
 
-var displayHotkeys = function () {
+const displayHotkeys = () => {
 	console.log('  Hotkeys:');
 	console.log();
 	console.log('    ESC - close the preview window and quit.');
 	console.log();
 };
 
-var themes = ['hand', 'simple'];
+const themes = ['hand', 'simple'];
 
-var displayThemes = function () {
+const displayThemes = () => {
 	console.log('  Themes (default is "hand"):');
 	console.log();
-	themes.forEach(function (theme) {
+	themes.forEach((theme) => {
 		console.log('    %s', theme);
 	});
 	console.log();
 };
 
-var displayErrorAndExit = function () {
+const displayErrorAndExit = (msg, ...args) => {
 	console.error();
-	console.error('  error: %s', util.format.apply(undefined, arguments));
+	console.error('  error: %s', msg, ...args);
 	console.error();
 	process.exit(1);
 };
 
-var validateTheme = function (theme) {
-	if (themes.contains(theme)) { return theme; }
-	displayErrorAndExit('theme not found: %s.', theme);
+const validateTheme = (theme) => {
+	if (themes.includes(theme)) { return theme; }
+	displayErrorAndExit(`theme not found: ${theme}`);
 };
 
 program
 	.version(require('./package.json').version)
 	.usage('[options] <file>')
-	.option('-s, --theme <style>', 'specify theme, default is "hand"', validateTheme)
+	.option('-s, --theme <theme>', 'specify theme, default is "hand"', validateTheme)
+	.option('-v, --verbose', 'display additional information (use multiple time to increase)', (_, total) => total + 1, 0)
+	.option('-d, --dev-tools', 'enable dev tools')
 	.on('--help', displayHotkeys)
 	.on('--help', displayThemes)
 	.parse(process.argv);
 
-if (program.args.length !== 1) { displayErrorAndExit('No single <file> specified.'); }
-if (!fs.existsSync(program.args[0])) { displayErrorAndExit('<file> not found: %s', program.args[0]); }
-var file = path.resolve(program.args[0]);
+const info = (msg, ...args) => {
+	if (program.verbose > 0) {
+		console.log(msg, ...args);
+	}
+};
 
-var env = process.env;
-env.LD_LIBRARY_PATH = path.join(__dirname, 'lib/');
-spawn(nw, [__dirname, program.theme || 'hand', file], { env: env });
+const debug = (msg, ...args) => {
+	if (program.verbose > 1) {
+		console.log(msg, ...args);
+	}
+};
+
+if (program.args.length !== 1) { displayErrorAndExit('No single <file> specified.'); }
+if (!fs.existsSync(program.args[0])) { displayErrorAndExit(`<file> not found: ${program.args[0]}`); }
+
+const file = path.resolve(program.args[0]);
+info('file: %s', file);
+const theme = program.theme || 'hand';
+info('theme: %s', theme);
+
+let win;
+const createWindow = () => {
+	win = new BrowserWindow({ center: true });
+	win.setMenu(null);
+	win.loadURL(`file://${__dirname}/index.html#theme=${theme}&file=${file}`);
+	win.on('closed', () => win = null);
+	win.webContents.on('before-input-event', (event, input) => {
+		if (input.type === 'keyDown' && input.code === 'Escape') { app.quit(); }
+	});
+	win.webContents.once('did-finish-load', update);
+	if (program.devTools) { win.webContents.openDevTools(); }
+};
+
+const update = () => {
+	fs.readFile(file, (err, data) => {
+		if (err) { return console.error(err); }
+		win.webContents.send('content', data.toString());
+	});
+}
+
+app.on('ready', () => {
+	createWindow();
+	fs.watch(file, (eventType, _) => {
+		debug('event: %s', eventType);
+		if (eventType === 'change') {
+			info('changed: %s', file);
+			update();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') { app.quit(); }
+});
+
+app.on('activate', () => {
+	if (!win) { createWindow(); }
+});
